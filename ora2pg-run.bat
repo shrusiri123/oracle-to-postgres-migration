bfcc1a8155279f5530360aaffa30bb3e3b30f880
shrusiri123 <shruti421994@gmail.com>
2026-06-29 07:20:50 +0530

    Fix ora2pg config - add ORACLE_HOME, update DSN to XEPDB1

---
+++ ora2pg-run.bat
@@ -0,0 +1,6 @@
+@echo off
+set ORACLE_HOME=C:\Shruti\dbhomeXE
+set ORACLE_BASE=C:\Shruti
+set NLS_LANG=AMERICAN_AMERICA.AL32UTF8
+set PATH=C:\Shruti\dbhomeXE\bin;C:\Strawberry\c\bin;C:\Strawberry\perl\site\bin;C:\Strawberry\perl\bin;C:\Windows\system32;C:\Windows;C:\Users\Shruti Sagar\AppData\Local\Programs\Python\Launcher
+perl C:\Strawberry\perl\site\bin\ora2pg %*
