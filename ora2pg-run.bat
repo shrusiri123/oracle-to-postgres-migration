57fd66cc7cc0482f5e01c9e725860521656e1365
shrusiri123 <shruti421994@gmail.com>
2026-06-29 07:23:53 +0530

    Updated the path to include Git path

--- ora2pg-run.bat
+++ ora2pg-run.bat
@@ -2,5 +2,5 @@
 set ORACLE_HOME=C:\Shruti\dbhomeXE
 set ORACLE_BASE=C:\Shruti
 set NLS_LANG=AMERICAN_AMERICA.AL32UTF8
-set PATH=C:\Shruti\dbhomeXE\bin;C:\Strawberry\c\bin;C:\Strawberry\perl\site\bin;C:\Strawberry\perl\bin;C:\Windows\system32;C:\Windows;C:\Users\Shruti Sagar\AppData\Local\Programs\Python\Launcher
+set PATH=C:\Shruti\dbhomeXE\bin;C:\Strawberry\c\bin;C:\Strawberry\perl\site\bin;C:\Strawberry\perl\bin;C:\Windows\system32;C:\Windows;C:\Users\Shruti Sagar\AppData\Local\Programs\Python\Launcher;C:\Program Files\Git\cmd
 perl C:\Strawberry\perl\site\bin\ora2pg %*
